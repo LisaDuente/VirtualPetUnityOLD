94e29dea091d1254985f2dc4358186f8656d2900
LisaDuente <lisa.duente@gmail.com>
2022-05-09 00:01:31 +0200

    Added curtains and changed brightness

---
+++ VirtualPet/Assets/SurroundingScripts/GardienenScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GardienenScript : MonoBehaviour
+{
+public Image image;
+public Sprite closed;
+public Sprite open;
+public GameObject background;
+private SpriteRenderer render;
+
+
+
+    void Start (){
+        render= background.GetComponent<SpriteRenderer>();
+    }
+
+  public void setImage(){
+      if(image.sprite != open){
+        image.sprite = open;
+        render.color = new Color(1,1,1,1);
+    
+      }else{
+        image.sprite = closed;
+        render.color = new Color(1,1,1,0.2f);
+      }
+      
+  }
+}
